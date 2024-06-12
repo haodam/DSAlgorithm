6eab1fe527e19cd31c62f1f1ab7633d03c8606da
haodam <damanhhao3004.com>
2024-06-13 01:56:00 +0700

    update: generics

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/redis/go-redis/v9 v9.3.0 // indirect
+	golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8 // indirect
 )
--- pkg/generics/main.go
+++ pkg/generics/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/exp/constraints"
 )
 
 func Add(a int, b int) int {
@@ -12,11 +13,29 @@ func AddGeneric[T int | float64](a T, b T) T {
 	return a + b
 }
 
+func AddGeneric2[T constraints.Ordered](a T, b T) T {
+	return a + b
+}
+
+func MapValues[T constraints.Ordered](values []T, mapFunc func(T) T) []T {
+	var newValues []T
+	for _, value := range values {
+		newValue := mapFunc(value)
+		newValues = append(newValues, newValue)
+	}
+	return newValues
+
+}
 func main() {
 
-	result := Add(1, 2)
-	result1 := AddGeneric(1.5, 2.9)
-
+	result := MapValues([]float64{1.1, 1.2, 1.3}, func(n float64) float64 {
+		return n * 2
+	})
 	fmt.Printf("result: %+v\n", result)
-	fmt.Printf("result1: %+v\n", result1)
+
+	//result := Add(1, 2)
+	//result1 := AddGeneric(1.5, 2.9)
+	//
+	//fmt.Printf("result: %+v\n", result)
+	//fmt.Printf("result1: %+v\n", result1)
 }
