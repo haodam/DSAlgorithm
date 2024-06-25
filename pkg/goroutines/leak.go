f3ae72bf8d9881aecc050833ee5c879b37750e36
haodam <damanhhao3004.com>
2024-06-25 17:17:01 +0700

    update: fix leak goroutine

--- pkg/goroutines/leak.go
+++ pkg/goroutines/leak.go
@@ -3,23 +3,53 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func LeakReceive() {
-	newRandStream := func() <-chan int {
+	newRandStream := func(done <-chan interface{}) <-chan int {
 		randStream := make(chan int)
 		go func() {
 			defer fmt.Println("newRandStream closure exited.") // (1)
 			defer close(randStream)
 			for {
+				select {
+				case randStream <- rand.Int():
+				case <-done:
+					return
+
+				}
 				randStream <- rand.Int()
 			}
 		}()
 		return randStream
 	}
-	randStream := newRandStream()
+
+	done := make(chan interface{})
+	randStream := newRandStream(done)
 	fmt.Println("3 random ints:")
 	for i := 1; i <= 3; i++ {
 		fmt.Printf("%d: %d\n", i, <-randStream)
 	}
+	close(done)
+	time.Sleep(1 * time.Second)
+}
+
+func Example2() {
+	start := time.Now()
+	c := make(chan interface{})
+	go func() {
+		time.Sleep(5 * time.Second)
+		close(c) // (1)
+	}()
+	fmt.Println("Blocking on read...")
+	select {
+	case <-c: // (2)
+		fmt.Printf("Unblocked %v later.\n", time.Since(start))
+	}
+}
+
+func main() {
+	//LeakReceive()
+	Example2()
 }
