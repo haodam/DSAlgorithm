4dc3468b245c41c64819706d281093017ec06e9a
damanhhao3004 <damanhhao3004@gmail.com>
2024-12-30 22:12:46 +0700

    update: add execute for async job

--- asyncjob/job.go
+++ asyncjob/job.go
@@ -12,6 +12,13 @@ import (
 // 3.   Should be stateful
 // 4.   We should have job manager to manage jobs (***)
 
+type Job interface {
+	Execute(ctx context.Context) error
+	Retry(ctx context.Context) error
+	State() JobState
+	SetRetryDuration(times []time.Duration)
+}
+
 const (
 	defaultMaxTimeout = 10 * time.Second
 )
@@ -20,7 +27,7 @@ var (
 	defaultRetryTime = []time.Duration{time.Second, time.Second * 5, time.Second * 10}
 )
 
-type JobHandler func(ctx context.Context, job *Job) error
+type JobHandler func(ctx context.Context) error
 
 type JobState int
 
@@ -28,13 +35,65 @@ const (
 	JobStateInt JobState = iota
 	JobStateRunning
 	JobStateFailed
+	JobStateTimeout
 	JobStateCompleted
 	JobStateRetryFailed
 )
 
-type Job interface {
-	Execute(ctx context.Context) error
-	Retry(ctx context.Context) error
-	State() JobState
-	SetRetryDuration(times []time.Duration)
+func (js JobState) String() string {
+	return []string{"Init", "Running", "Failed", "Timeout", "Completed"}[js]
+}
+
+type JobConfig struct {
+	MaxTimeout time.Duration
+	Retries    []time.Duration
+}
+
+type job struct {
+	config     JobConfig
+	handler    JobHandler
+	state      JobState
+	retryIndex int
+	stopChan   chan bool
+}
+
+func NewJob(handler JobHandler) *job {
+	j := job{
+		config: JobConfig{
+			MaxTimeout: defaultMaxTimeout,
+			Retries:    defaultRetryTime,
+		},
+		handler:    handler,
+		state:      JobStateInt,
+		retryIndex: -1, // chua retry lan nao het
+		stopChan:   make(chan bool),
+	}
+	return &j
+}
+
+func (j *job) Execute(ctx context.Context) error {
+
+	j.state = JobStateInt
+	var err error
+	err = j.handler(ctx)
+
+	if err != nil {
+		j.state = JobStateFailed
+		return err
+	}
+
+	j.state = JobStateCompleted
+	return nil
+}
+
+func (j *job) Retry(ctx context.Context) error {
+	return nil
+}
+
+func (j *job) State() JobState {
+	return 0
+}
+
+func (j *job) SetRetryDuration(times []time.Duration) {
+
 }
