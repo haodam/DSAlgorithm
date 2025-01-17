5fa672d74340356629428d29939c6e22afe1f1f6
damanhhao3004 <damanhhao3004@gmail.com>
2025-01-17 08:34:47 +0700

    feat: leetcode two sum ID =1

--- asyncjob/example/main.go
+++ asyncjob/example/main.go
@@ -17,17 +17,38 @@ func main() {
 		return errors.New("something went wrong at job 1")
 	})
 
-	if err := job1.Execute(context.Background()); err != nil {
-		log.Println(job1.State(), err)
+	job2 := asyncjob.NewJob(func(ctx context.Context) error {
+		time.Sleep(time.Second * 2)
+		log.Println("I am job 2")
+
+		return nil
+		//return errors.New("something went wrong at job 2")
+	})
+
+	job3 := asyncjob.NewJob(func(ctx context.Context) error {
+		time.Sleep(time.Second * 3)
+		log.Println("I am job 3")
+
+		return nil
+		//return errors.New("something went wrong at job 3")
+	})
+
+	group := asyncjob.NewGroup(false, job1, job2, job3)
+	if err := group.Run(context.Background()); err != nil {
+		log.Println(err)
 	}
 
-	for {
-		if err := job1.Retry(context.Background()); err != nil {
-			log.Println(err)
-		}
-		if job1.State() == asyncjob.JobStateRetryFailed || job1.State() == asyncjob.JobStateCompleted {
-			log.Println(job1.State())
-			break
-		}
-	}
+	//if err := job1.Execute(context.Background()); err != nil {
+	//	log.Println(job1.State(), err)
+	//}
+	//
+	//for {
+	//	if err := job1.Retry(context.Background()); err != nil {
+	//		log.Println(err)
+	//	}
+	//	if job1.State() == asyncjob.JobStateRetryFailed || job1.State() == asyncjob.JobStateCompleted {
+	//		log.Println(job1.State())
+	//		break
+	//	}
+	//}
 }
